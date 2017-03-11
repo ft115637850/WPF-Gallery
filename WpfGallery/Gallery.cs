a5a4ce7d1ec90666d3a86dd4c1176f8125d10ce5
ft115637850 <gentleman000000@hotmail.com>
2017-03-11 14:30:01 +0800

    Initialize the project

---
+++ WpfGallery/Gallery.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+
+namespace WpfGallery
+{
+    [TemplatePart(Name = "PART_Pane1", Type = typeof(ImgPanel))]
+    [TemplatePart(Name = "PART_Pane1ImagePlaceholder", Type = typeof(Image))]
+    [TemplatePart(Name = "PART_Pane1border", Type = typeof(DropShadowEffect))]
+    [TemplatePart(Name = "PART_Pane2", Type = typeof(ImgPanel))]
+    [TemplatePart(Name = "PART_Pane2ImagePlaceholder", Type = typeof(Image))]
+    [TemplatePart(Name = "PART_Pane2border", Type = typeof(DropShadowEffect))]
+    [TemplatePart(Name = "PART_Pane3", Type = typeof(ImgPanel))]
+    [TemplatePart(Name = "PART_Pane3ImagePlaceholder", Type = typeof(Image))]
+    [TemplatePart(Name = "PART_Pane3border", Type = typeof(DropShadowEffect))]
+    public class Gallery : Control
+    {
+        #region Static Fields
+        public static DependencyProperty ImgsSrcProperty;
+
+        static Gallery()
+        {
+            ImgsSrcProperty = DependencyProperty.Register(
+                            "ImgsSrc",
+                            typeof(List<BitmapSource>),
+                            typeof(Gallery),
+                            new FrameworkPropertyMetadata(new List<BitmapSource>(), new PropertyChangedCallback(OnImgsSrcChanged)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Gallery), new FrameworkPropertyMetadata(typeof(Gallery)));
+        }
+
+        private static void OnImgsSrcChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Gallery colorPicker = (Gallery)sender;
+            colorPicker.ImgsSrc = (List<BitmapSource>)e.NewValue;
+        }
+        #endregion
+
+        #region Instance Fields
+        private RelayCommand rotateCommand;
+        #endregion
+
+        #region Instance Propertie
+        public List<BitmapSource> ImgsSrc
+        {
+            get { return (List<BitmapSource>)GetValue(ImgsSrcProperty); }
+            set { SetValue(ImgsSrcProperty, value); }
+        }
+
+        internal List<ImgPanel> Panels
+        {
+            set;
+            get;
+        }
+
+        public RelayCommand RotateCommand
+        {
+            get
+            {
+                if (rotateCommand == null)
+                {
+                    rotateCommand = new RelayCommand(Rotate);
+                }
+                return rotateCommand;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Gallery()
+        {
+            this.DataContext = this;
+        }
+        #endregion
+
+        #region Public Instance Methods
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var pane1 = GetTemplateChild("PART_Pane1") as ImgPanel;
+            var pane2 = GetTemplateChild("PART_Pane2") as ImgPanel;
+            var pane3 = GetTemplateChild("PART_Pane3") as ImgPanel;
+            if (pane1 != null && pane2 != null && pane3 != null)
+            {
+                //TO DO: Command
+                //pane1.MouseDown += ImagePlaceholder_MouseDown;
+                //pane2.MouseDown += ImagePlaceholder_MouseDown;
+                //pane3.MouseDown += ImagePlaceholder_MouseDown;
+                Panels = new List<ImgPanel>
+                        {
+                            pane1,
+                            pane2,
+                            pane3
+                        };
+            }
+
+            //this.Unloaded += (s, a) => 
+            //{
+            //    pane1.MouseDown -= ImagePlaceholder_MouseDown;
+            //    pane2.MouseDown -= ImagePlaceholder_MouseDown;
+            //    pane3.MouseDown -= ImagePlaceholder_MouseDown;
+            //};
+        }
+
+        //public void Dispose()
+        //{
+        //    foreach (var pane in Panels)
+        //    {
+        //        pane.MouseDown -= ImagePlaceholder_MouseDown;
+        //    }
+        //}
+        #endregion
+
+        #region Private Instance Methods
+        private void Rotate(object position)
+        {
+            var panelPosition = (ImagePanelPosition)position;
+            switch (panelPosition)
+            {
+                case ImagePanelPosition.Left:
+                    this.AnticlockwiseNav();
+                    break;
+                case ImagePanelPosition.Middle:
+                    //Raise event
+                    break;
+                case ImagePanelPosition.Right:
+                    this.ClockWiseNav();
+                    break;
+            }
+        }
+
+        private void ClockWiseNav()
+        {
+            var sb = new Storyboard();
+            foreach (var panel in Panels)
+            {
+                var animations = panel.GetClockWiseNavAnimations();
+                foreach (var animation in animations)
+                {
+                    sb.Children.Add(animation);
+                }
+
+                panel.ClockWiseNav();
+            }
+
+            sb.Begin(this);
+        }
+
+        private void AnticlockwiseNav()
+        {
+            var sb = new Storyboard();
+            foreach (var panel in Panels)
+            {
+                var animations = panel.GetAnticlockwiseNavAnimations();
+                foreach (var animation in animations)
+                {
+                    sb.Children.Add(animation);
+                }
+
+                panel.AnticlockwiseNav();
+            }
+
+            sb.Begin(this);
+        }
+
+        #endregion
+        
+    }
+}
---
+++ WpfGallery/ImgPanel.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+
+namespace WpfGallery
+{
+    internal enum ImagePanelPosition
+    {
+        Left,
+        Middle,
+        Right
+    }
+
+    internal class ImgPanel : DockPanel
+    {
+        #region Static Fields
+        public static DependencyProperty PositionProperty;
+        static ImgPanel()
+        {
+            PositionProperty = DependencyProperty.Register(
+                            "Position",
+                            typeof(ImagePanelPosition),
+                            typeof(ImgPanel),
+                            new FrameworkPropertyMetadata(ImagePanelPosition.Middle, new PropertyChangedCallback(OnPositionChanged)));
+        }
+
+        private static void OnPositionChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ImgPanel panel = (ImgPanel)sender;
+            panel.Position = (ImagePanelPosition)e.NewValue;
+            /*
+            switch (panel.Position)
+            {
+                case ImagePanelPosition.Left:
+                    panel.translate.X = -300;
+                    panel.translate.Y = -10;
+                    panel.scale.ScaleX = 0.7;
+                    panel.scale.ScaleY = 0.7;
+                    break;
+                case ImagePanelPosition.Middle:
+                    panel.translate.X = 1;
+                    panel.translate.Y = 1;
+                    panel.scale.ScaleX = 1;
+                    panel.scale.ScaleY = 1;
+                    break;
+                case ImagePanelPosition.Right:
+                    panel.translate.X = 500;
+                    panel.translate.Y = -10;
+                    panel.scale.ScaleX = 0.7;
+                    panel.scale.ScaleY = 0.7;
+                    break;
+            }*/
+        }
+
+        #endregion
+
+        #region Instance Fields
+        private Int32AnimationUsingKeyFrames zIndexAnimation;
+        private DoubleAnimation translateXAnimation;
+        private DoubleAnimation translateYAnimation;
+        private DoubleAnimation scaleXAnimation;
+        private DoubleAnimation scaleYAnimation;
+        private DoubleAnimation borderAnimation;
+        #endregion
+
+        #region Instance Propertie
+        public ImagePanelPosition Position
+        {
+            get { return (ImagePanelPosition)GetValue(PositionProperty); }
+            set { SetValue(PositionProperty, value); }
+        }
+
+        #endregion
+
+        #region Public Instance Methods
+        public void ClockWiseNav()
+        {
+            switch (this.Position)
+            {
+                case ImagePanelPosition.Left:
+                    this.Position = ImagePanelPosition.Right;
+                    break;
+                case ImagePanelPosition.Middle:
+                    this.Position = ImagePanelPosition.Left;
+                    break;
+                case ImagePanelPosition.Right:
+                    this.Position = ImagePanelPosition.Middle;
+                    break;
+            }
+        }
+
+        public void AnticlockwiseNav()
+        {
+            switch (this.Position)
+            {
+                case ImagePanelPosition.Left:
+                    this.Position = ImagePanelPosition.Middle;
+                    break;
+                case ImagePanelPosition.Middle:
+                    this.Position = ImagePanelPosition.Right;
+                    break;
+                case ImagePanelPosition.Right:
+                    this.Position = ImagePanelPosition.Left;
+                    break;
+            }
+        }
+        
+        public List<AnimationTimeline> GetClockWiseNavAnimations()
+        {
+            switch (this.Position)
+            {
+                case ImagePanelPosition.Left:
+                    this.UpdateAnimationForNewRightComing();
+                    break;
+                case ImagePanelPosition.Middle:
+                    this.UpdateAnimationForMiddleToLeft();
+                    break;
+                case ImagePanelPosition.Right:
+                    this.UpdateAnimationForRightToMiddle();
+                    break;
+            }
+
+            return new List<AnimationTimeline>() 
+            { 
+                this.zIndexAnimation,
+                this.translateXAnimation,
+                this.translateYAnimation,
+                this.scaleXAnimation,
+                this.scaleYAnimation,
+                this.borderAnimation
+            };
+        }
+        
+        public List<AnimationTimeline> GetAnticlockwiseNavAnimations()
+        {
+            switch (this.Position)
+            {
+                case ImagePanelPosition.Left:
+                    this.UpdateAnimationForLeftToMiddle();
+                    break;
+                case ImagePanelPosition.Middle:
+                    this.UpdateAnimationForMiddleToRight();
+                    break;
+                case ImagePanelPosition.Right:
+                    this.UpdateAnimationForNewLeftComing();
+                    break;
+            }
+
+            return new List<AnimationTimeline>() 
+            { 
+                this.zIndexAnimation,
+                this.translateXAnimation,
+                this.translateYAnimation,
+                this.scaleXAnimation,
+                this.scaleYAnimation,
+                this.borderAnimation
+            };
+        }
+        #endregion
+
+        #region Protected Instance Methods
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            this.RenderTransform = new TransformGroup()
+            {
+                Children =
+                {
+                    new TranslateTransform()
+                    {
+                        X = this.Position == ImagePanelPosition.Left ? -300 : (this.Position == ImagePanelPosition.Middle ? 0 : 500),
+                        Y = this.Position == ImagePanelPosition.Middle ? 0 : -10
+                    },
+                    new ScaleTransform()
+                    {
+                        ScaleX = this.Position == ImagePanelPosition.Middle ? 1 : 0.7,
+                        ScaleY = this.Position == ImagePanelPosition.Middle ? 1 : 0.7
+                    }
+                }
+            };
+
+            this.zIndexAnimation = new Int32AnimationUsingKeyFrames();
+            Storyboard.SetTarget(this.zIndexAnimation, this);
+            Storyboard.SetTargetProperty(this.zIndexAnimation, new PropertyPath(ImgPanel.ZIndexProperty));
+
+            this.translateXAnimation = new DoubleAnimation();
+            this.translateXAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            Storyboard.SetTarget(this.translateXAnimation, this);
+            Storyboard.SetTargetProperty(this.translateXAnimation, new PropertyPath("RenderTransform.Children[0].X"));
+
+            this.translateYAnimation = new DoubleAnimation();
+            this.translateYAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            Storyboard.SetTarget(this.translateYAnimation, this);
+            Storyboard.SetTargetProperty(this.translateYAnimation, new PropertyPath("RenderTransform.Children[0].Y"));
+
+            this.scaleXAnimation = new DoubleAnimation();
+            this.scaleXAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            Storyboard.SetTarget(this.scaleXAnimation, this);
+            Storyboard.SetTargetProperty(this.scaleXAnimation, new PropertyPath("RenderTransform.Children[1].ScaleX"));
+
+            this.scaleYAnimation = new DoubleAnimation();
+            this.scaleYAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            Storyboard.SetTarget(this.scaleYAnimation, this);
+            Storyboard.SetTargetProperty(this.scaleYAnimation, new PropertyPath("RenderTransform.Children[1].ScaleY"));
+
+            var shadow = this.FindName(this.Name + "border") as DropShadowEffect;
+            this.borderAnimation = new DoubleAnimation();
+            this.borderAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            Storyboard.SetTarget(this.borderAnimation, shadow);
+            Storyboard.SetTargetProperty(this.borderAnimation, new PropertyPath(DropShadowEffect.ShadowDepthProperty));
+ 
+        }
+
+        #endregion     
+
+        #region Private Methods
+        private void UpdateAnimationForNewRightComing()
+        {
+            this.zIndexAnimation.KeyFrames = new Int32KeyFrameCollection 
+                    {
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0), Value = -2},
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0.8), Value = -1}
+                    };
+
+            this.translateXAnimation.From = 1000;
+            this.translateXAnimation.To = 500;
+
+            this.translateYAnimation.From = -100;
+            this.translateYAnimation.To = -10;
+
+            this.scaleXAnimation.From = 0.4;
+            this.scaleXAnimation.To = 0.7;
+
+            this.scaleYAnimation.From = 0.4;
+            this.scaleYAnimation.To = 0.7;
+
+            this.borderAnimation.From = 0;
+            this.borderAnimation.To = 1;
+        }
+
+        private void UpdateAnimationForMiddleToLeft()
+        {
+            this.zIndexAnimation.KeyFrames = new Int32KeyFrameCollection 
+                    {
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0), Value = 1},
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0.8), Value = 0}
+                    };
+
+            this.translateXAnimation.From = 0;
+            this.translateXAnimation.To = -300;
+
+            this.translateYAnimation.From = 0;
+            this.translateYAnimation.To = -10;
+
+            this.scaleXAnimation.From = 1;
+            this.scaleXAnimation.To = 0.7;
+
+            this.scaleYAnimation.From = 1;
+            this.scaleYAnimation.To = 0.7;
+
+            this.borderAnimation.From = 2;
+            this.borderAnimation.To = 1;
+        }
+
+        private void UpdateAnimationForRightToMiddle()
+        {
+            this.zIndexAnimation.KeyFrames = new Int32KeyFrameCollection 
+                    {
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0), Value = -1},
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0.8), Value = 1}
+                    };
+
+            this.translateXAnimation.From = 500;
+            this.translateXAnimation.To = 0;
+
+            this.translateYAnimation.From = -10;
+            this.translateYAnimation.To = 0;
+
+            this.scaleXAnimation.From = 0.7;
+            this.scaleXAnimation.To = 1;
+
+            this.scaleYAnimation.From = 0.7;
+            this.scaleYAnimation.To = 1;
+
+            this.borderAnimation.From = 1;
+            this.borderAnimation.To = 2;
+        }
+
+        private void UpdateAnimationForLeftToMiddle()
+        {
+            this.zIndexAnimation.KeyFrames = new Int32KeyFrameCollection 
+                    {
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0), Value = -1},
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0.8), Value = 1}
+                    };
+
+            this.translateXAnimation.From = -300;
+            this.translateXAnimation.To = 0;
+
+            this.translateYAnimation.From = -10;
+            this.translateYAnimation.To = 0;
+
+            this.scaleXAnimation.From = 0.7;
+            this.scaleXAnimation.To = 1;
+
+            this.scaleYAnimation.From = 0.7;
+            this.scaleYAnimation.To = 1;
+
+            this.borderAnimation.From = 1;
+            this.borderAnimation.To = 2;
+        }
+
+        private void UpdateAnimationForMiddleToRight()
+        {
+            this.zIndexAnimation.KeyFrames = new Int32KeyFrameCollection 
+                    {
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0), Value = 1},
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0.8), Value = 0}
+                    };
+
+            this.translateXAnimation.From = 0;
+            this.translateXAnimation.To = 500;
+
+            this.translateYAnimation.From = 0;
+            this.translateYAnimation.To = -10;
+
+            this.scaleXAnimation.From = 1;
+            this.scaleXAnimation.To = 0.7;
+
+            this.scaleYAnimation.From = 1;
+            this.scaleYAnimation.To = 0.7;
+
+            this.borderAnimation.From = 2;
+            this.borderAnimation.To = 1;
+        }
+
+        private void UpdateAnimationForNewLeftComing()
+        {
+            this.zIndexAnimation.KeyFrames = new Int32KeyFrameCollection 
+                    {
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0), Value = -2},
+                        new SplineInt32KeyFrame(){ KeyTime = TimeSpan.FromSeconds(0.8), Value = -1}
+                    };
+
+            this.translateXAnimation.From = -500;
+            this.translateXAnimation.To = -300;
+
+            this.translateYAnimation.From = -50;
+            this.translateYAnimation.To = -10;
+
+            this.scaleXAnimation.From = 0.4;
+            this.scaleXAnimation.To = 0.7;
+
+            this.scaleYAnimation.From = 0.4;
+            this.scaleYAnimation.To = 0.7;
+
+            this.borderAnimation.From = 0;
+            this.borderAnimation.To = 1;
+        }
+        #endregion
+    }
+}
