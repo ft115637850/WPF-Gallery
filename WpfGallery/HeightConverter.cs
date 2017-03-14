88c383da60569388c7777e378f867f11b7c78f73
ft115637850 <gentleman000000@hotmail.com>
2017-03-14 16:12:54 +0800

    Tune width and height

---
+++ WpfGallery/HeightConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfGallery
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    public class HeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double totalHeight = (double)value;
+            return totalHeight * 3 / 5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
